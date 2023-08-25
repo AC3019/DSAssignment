183569864c6969196912a2ff8f38c0a572ce7bc0
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2023-08-25 11:06:41 +0800

    Add functional interface filterable

---
+++ src/utility/Filterable.java
@@ -0,0 +1,16 @@
+package utility;
+
+@FunctionalInterface
+/**
+ * Filterable executes the function `apply` to determine whether the passed in item should be included in the final filtered result, and is normally used in specific class's `filter` method
+ */
+public interface Filterable<T> {
+    
+    /**
+     * apply accepts an item of any type
+     * @param item
+     * @return true if the item passes the filter condition, false if it should be filtered out
+     */
+    public abstract boolean apply(T item);
+
+}
