492311fb28b89b9132b7a7f2c5bb74e31931205e
AC3019 <55951264+AC3019@users.noreply.github.com>
2023-08-27 18:51:51 +0800

    implement fucntions and UI required

--- src/entity/TutorialGroup.java
+++ src/entity/TutorialGroup.java
@@ -3,14 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entity;
-
+import adt.ArrayList;
 /**
  *
  * @author Neoh Soon Chee
  */
 public class TutorialGroup {
     private String tutGrpCode;
-    private Student[] student;
+    private ArrayList<Student> student;
     
     public TutorialGroup (String tutGrpCode){
         this.tutGrpCode = tutGrpCode;
@@ -20,7 +20,11 @@ public class TutorialGroup {
         this.tutGrpCode = tutGrpCode;
     }
     
-    public String gettutGrpCode(){
+    public String getTutGrpCode(){
         return tutGrpCode;
+    }
+    
+    public ArrayList<Student> getStudent(){
+        return student;
     }
 }
