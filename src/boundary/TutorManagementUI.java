166ae312faef2bbe8b8fe78308a27a122bf293a5
yongyk <yikityong@gmail.com>
2023-08-28 14:49:42 +0800

    update all files

--- src/boundary/TutorManagementUI.java
+++ src/boundary/TutorManagementUI.java
@@ -9,6 +9,9 @@ package boundary;
  *
  * @author yong
  */
+import utility.Input;
+import entity.Tutor;
+import java.util.Scanner;
 public class TutorManagementUI {
     public int printMenu() {
         System.out.println("1. Add a new tutor");
@@ -27,7 +30,43 @@ public class TutorManagementUI {
                     "Amend tutor details"
             },
             (item) -> item
-        )
-        
+        );
+         
+    }
+    public Tutor tutorInput(){
+        Scanner scan = new Scanner(System.in);
+        boolean invalidInput = false;
+        String name = "";
+        String subject = "";
+        char gender;
+        int age;
+        String phoneNum;
+        do {
+            invalidInput = false;
+            System.out.print("Name: ");
+            name = scan.nextLine();
+
+            System.out.print("Subject: ");
+            subject = scan.nextLine();
+
+            System.out.print("Gender(M/F): ");
+            gender = scan.next().toUpperCase().charAt(0);
+
+            System.out.print("Age: ");
+            age = scan.nextInt();
+            scan.nextLine();// clear buffer
+            System.out.print("Phone: ");
+            phoneNum = scan.nextLine();
+
+            // validate gender
+            if (Character.valueOf(gender).compareTo("M".charAt(0)) != 0
+                    && Character.valueOf(gender).compareTo("F".charAt(0)) != 0) {
+                System.out.println("Gender should be either M or F. Please re-enter...");
+                invalidInput = true;
+            }
+             } while (invalidInput);
+
+        Tutor tutorObj = new Tutor(getId(), name, subject, gender, age, phoneNum);
+        return tutorObj;
     }
 }
--- src/control/TutorManagement.java
+++ src/control/TutorManagement.java
@@ -8,14 +8,38 @@ package control;
  *
  * @author yong
  */
+import entity.Tutor;
+import adt.HashMap;
+import adt.MapInterface;
+import boundary.TutorManagementUI;
 public class TutorManagement {
-
-    /**
-     * @param args the command line arguments
-     */
+    MapInterface<Integer, Tutor> tutors = new HashMap<>();
+    Tutor tutorObj=new Tutor();
     public static void main(String[] args) {
         TutorManagement tm = new TutorManagement();
         
+    }
+    public static void addNewTutor(MapInterface<Integer,Tutor>tutors){
+         if (tutors.size() > 0) {
+            boolean containsDuplicate = false;
+            for (MapInterface.Entry<Integer, Tutor> tutor : tutors.entrySet()) {
+                if (tutor.getValue().getName().equals(tutorObj.getName())) {// Duplicate name
+                    containsDuplicate = true;
+                    break;
+                }
+            }
+
+            if (containsDuplicate) {
+                System.out.println("This tutor already exists in the list.");
+            } else {
+                tutors.put(id, tutorObj);
+                id++;
+            }
+        } else {
+            // Will only come here for first tutor addition
+            tutors.put(id, tutorObj);
+            id++;
+        }
     }
     
 }
