b702f891752ca870a6d61c07c08376b1915232a6
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2023-09-01 09:36:29 +0800

    Add department stub

--- src/control/CourseManagement.java
+++ src/control/CourseManagement.java
@@ -1,5 +1,7 @@
 package control;
 
+import java.io.Serializable;
+
 import adt.ArrayList;
 import entity.Course;
 
@@ -7,10 +9,10 @@ import entity.Course;
  * Just a stub to simulate the course module, since teaching assignment needs it
  *  @author xuan bin
  */
-public class CourseManagement {
+public class CourseManagement implements Serializable {
     private ArrayList<Course> courses = new ArrayList<>(new Course[] {
 
-    }); 
+    });
     
     public ArrayList<Course> getCourses() {
         return this.courses;
---
+++ src/control/DepartmentManagement.java
@@ -0,0 +1,11 @@
+package control;
+
+/**
+ * Just a stub, to let course and tutor choose department
+ */
+public class DepartmentManagement {
+    public static String[] departments = new String[] {
+        "FOCS",
+        "FAFB"
+    };
+}
