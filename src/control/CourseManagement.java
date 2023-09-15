55944d283dc3cf98e10c0a1792ae7becbbf135d1
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2023-09-15 18:33:20 +0800

    Add more courses

--- src/control/CourseManagement.java
+++ src/control/CourseManagement.java
@@ -14,6 +14,9 @@ public class CourseManagement implements Serializable {
         new Course("BACS 2023", "Data Structures and Algorithms", DepartmentManagement.departments[0], 3),
         new Course("BACS 1023", "Introduction to Computing", DepartmentManagement.departments[0], 3),
         new Course("BACS 1014", "Introduction to Computer Systems", DepartmentManagement.departments[0], 4),
+        new Course("BAEN 1013", "Introduction to English", DepartmentManagement.departments[4], 3),
+        new Course("BAAF 1014", "Introduction to Accounting", DepartmentManagement.departments[1], 4),
+        new Course("BAET 1014", "Introduction to Electronic Engineering", DepartmentManagement.departments[2], 4),
     });
     
     public ArrayList<Course> getCourses() {
